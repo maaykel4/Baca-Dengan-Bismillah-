f8036518f4a378e949953eb6a1cebf6f66713259
maaykel4 <azkatetsuya@gmail.com>
2024-06-01 00:09:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let nextClicked = false;
 nextBtn.addEventListener("click", () => {
   if (!nextClicked) {
     question.innerHTML = "Selamat Bulan Juni,ya! Kalau kata Pak Sapardi, 'Tak ada yang lebih tabah dari Hujan di Bulan Juni' semoga lo bisa setabah itu menghadapi sisa-sisa waktu Kadin di Bulan Juni ini, walaupun kita udah ngga berteman, but i'll be ur number one supporter di saat semua orang Kadin memusuhi lo karena aneh. Deni Manusia Ikan, makasih banyak udah membantu gua melewati masa-masa sulit dua minggu terakhir, it was my lowest point in my life, ngga tau kalo lo gak bantu memberi distraksi gua akan sehancur apa. Lo keren, se-worth it itu, jangan pernah berkecil hati karena yang kecil hanya gaji intern Kadin. Semoga kita bisa mereview Kopi Tuku sambil memijakkan kaki di Bulan! #ApresiasiGueYangSeharianBelajarNgodingBuatIni";
-    gif.src = "https://github.com/DzarelDeveloper/Img/blob/main/okey.gif";
+    gif.src = "https://github.com/DzarelDeveloper/Img/blob/main/MauTidur.gif";
     nextBtn.innerHTML = "reply";
     nextClicked = true;
   } else {
